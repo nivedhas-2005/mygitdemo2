40d4938f8de5cd8ec3665da07afee41d1c463fd0
NIVEDHA <NIVEDHA@NIVE>
2024-11-27 22:52:11 +0530

    First Version

---
+++ mygitdemo2/src/test/java/com/infosys/mygitdemo2/Mygitdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.mygitdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mygitdemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
